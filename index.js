577f1d09c674a192f4842292c752f47b90dcd08a
Lin <linlincc@gmail.com>
2022-07-27 20:19:47 -0400

    header and image rounded

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Select some elements...
+// change the header to left
+
+let header = document.querySelector ("#page-header")
+header.style.textAlign = "left"
+
+//change the dog images
+
+let dogImages = document.querySelectorAll (".dog-image")
+for (let i=0; i < dogImages.length; i++) {
+ dogImages[i].style.borderRadius = "25pt"
+}
+
